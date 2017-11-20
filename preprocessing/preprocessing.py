dc5f5f090ff4b2f7691c39fec02c45d66f7e868c
Schneipi <pitschneider@icloud.com>
2017-11-20 19:41:56 +0100

    Add files via upload

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,38 +1,42 @@
-# dora's version with some small modifications
-# - fileName and targetFrequency as parameters
-# - actual frequency is not hardcoded, but extracted from .info file
-
 import numpy
+import csv
 from scipy import signal
 import scipy.io as sio
 import matplotlib.pyplot as plt
 
-def preproc(fileName, targetFrequency=250):
+def preproc(fileName, targetFrequency=250, targetSeconds=1):
+	
 	# load mat file
 	input = sio.loadmat(fileName + '.mat')
 	x = input['val']
+	
 	# transpose x to a column vector
 	xt = numpy.transpose(x)
+	
 	# change frequency
-	secs = (len(xt) / getActualFrequency(fileName)) * targetFrequency
-	xnf = signal.resample(xt, secs)
+	numberOfValues = (len(xt) / getActualFrequency(fileName)) * targetFrequency
+	xnf = signal.resample(xt, numberOfValues)
+			
 	# rescale the signal
 	mn = numpy.min(xnf)
 	mx = numpy.max(xnf)
 	rsc = (xnf - mn) / (mx - mn)
-	# split the signal to 10 equal signals
-	xfinal = numpy.vsplit(rsc, 10)
-	a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 = xfinal
-	#just for checking
-	print(xfinal)
-	plt.plot(a1,'r')
-	plt.show()
 	
+	# split the signal to targetLength
+	numberOfParts = numberOfValues / targetFrequency / targetSeconds
+	counter = 0
+	for subarray in numpy.vsplit(rsc, 10):
+		numpy.savetxt(fileName + str(counter) + '.csv', subarray, delimiter=',')
+		counter = counter + 1
+		
 def getActualFrequency(fileName):
+	
 	info = open(fileName + '.info','r')
 	text = info.read()
 	start = 'Sampling frequency: '
 	end = 'Hz'
 	return int((text.split(start))[1].split(end)[0])
 
+# can be used with a .mat file of any signal length, frequency and number of leadss
+# output will be presented in several .csv files
 preproc('100m')
